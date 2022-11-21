aebbc1e407e82b306ee7499b86185c07ed968251
neaguIF21 <neagu.ionut0412@gmail.com>
2022-11-21 13:31:13 +0200

    nav/hero/services

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
-let cf 
-cf = "ce faci?"
+// /navigation
 
-console.log(cf)
+let toogleButton = document.querySelector("#nav-icon4")
+let navbarLinks = document.querySelector(".navbar-links")
+
+toogleButton.addEventListener("click",()=>{
+    navbarLinks.classList.toggle('active')
+    toogleButton.classList.toggle('open')
+})
+
+navbarLinks.addEventListener("click",()=>{
+  navbarLinks.classList.toggle('active')
+  toogleButton.classList.toggle('open')
+})
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
